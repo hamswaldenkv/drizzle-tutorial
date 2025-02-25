358e829aa1263ac54bfd94527131d2db9a8fd569
Hams Kuveluka <kuvelukahams@gmail.com>
2025-02-25 15:31:48 +0100

    Added post api creation support

---
+++ src/app/api/posts/route.ts
@@ -0,0 +1,50 @@
+import db from "@/db";
+import { Post, User } from "@/db/schemas";
+import { eq } from "drizzle-orm";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const page = searchParams.get("page") || "1";
+  const perPage = searchParams.get("perPage") || "10";
+
+  return NextResponse.json({ message: "" });
+}
+
+export async function POST(req: Request) {
+  const { title, content, author_id, published } = await req.json();
+
+  try {
+    const usersResult = await db
+      .select()
+      .from(User)
+      .where(eq(User.id, author_id));
+    const [user] = usersResult;
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const post = await db
+      .insert(Post)
+      .values({
+        title,
+        content,
+        published: Boolean(published),
+        authorId: user.id,
+        slug: slugifyText(title),
+      })
+      .returning()
+      .then((e) => e[0]);
+
+    return NextResponse.json({ post });
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
+}
+
+export const slugifyText = (text: string) => {
+  return text.toLowerCase().replace(/ /g, "-");
+};
